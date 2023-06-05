a9a188d2bb8fd14d67a18aedc2f7bf6f346f2925
Aaron Manaloto <mail@aapomm.io>
2023-06-05 17:48:58 +0800

    Use cweid as plugin_id

--- lib/dradis/plugins/veracode/importer.rb
+++ lib/dradis/plugins/veracode/importer.rb
@@ -7,12 +7,12 @@ module Dradis::Plugins::Veracode
     # The framework will call this function if the user selects this plugin from
     # the dropdown list and uploads a file.
     # @returns true if the operation was successful, false otherwise
-    def import(params={})
-      file_content = File.read( params[:file] )
+    def import(params = {})
+      file_content = File.read(params[:file])
 
       # Parse the uploaded file into a Ruby Hash
       logger.info { "Parsing Veracode output from #{ params[:file] }..." }
-      xml = Nokogiri::XML( file_content )
+      xml = Nokogiri::XML(file_content)
       logger.info { 'Done.' }
 
       # Do a sanity check to confirm the user uploaded the right file
@@ -29,7 +29,7 @@ module Dradis::Plugins::Veracode
 
       # parse each severity > category > cwe > flaws
       xml.root.xpath('./xmlns:severity').each do |xml_severity|
-        logger.info{ "\t => Severity (level: #{ xml_severity[:level] })" }
+        logger.info { "\t => Severity (level: #{ xml_severity[:level] })" }
         xml_severity.xpath('.//xmlns:flaw').each do |xml_flaw|
           parse_flaw(xml_flaw)
         end
@@ -42,7 +42,7 @@ module Dradis::Plugins::Veracode
     def parse_report_details(xml_detailedreport)
       app_name = xml_detailedreport[:app_name]
       app_node = content_service.create_node(label: app_name)
-      logger.info{ "Adding report details (app_name: #{ app_name })" }
+      logger.info { "Adding report details (app_name: #{ app_name })" }
 
       [
         :app_id, :business_criticality, :business_owner, :business_unit,
@@ -54,13 +54,12 @@ module Dradis::Plugins::Veracode
       app_node.save
     end
 
-
     def parse_flaw(xml_flaw)
-      flaw_id = xml_flaw[:issueid]
-      logger.info{ "\t\t => Creating new issue (flaw issueid: #{ flaw_id })" }
+      cwe_id = xml_flaw[:cweid]
+      logger.info { "\t\t => Creating new issue (flaw issueid: #{ cwe_id })" }
 
       issue_text = template_service.process_template(template: 'issue', data: xml_flaw)
-      issue = content_service.create_issue(text: issue_text, id: flaw_id)
+      issue = content_service.create_issue(text: issue_text, id: cwe_id)
     end
   end
 end
