6d074725dd8783dfa937c85b76ab5bc821f6a66b
Matt Budz <matt.budz@protonmail.com>
2024-03-18 19:37:12 +0100

    use constant for default mappings

--- lib/dradis/plugins/veracode/mapping.rb
+++ lib/dradis/plugins/veracode/mapping.rb
@@ -1,27 +1,24 @@
 module Dradis::Plugins::Veracode
   module Mapping
-
-    def self.default_mapping
-      {
-        'evidence' => {
-          'Description' => '{{ veracode[evidence.description] }}',
-          'LineNumber' => '{{ veracode[evidence.line] }}',
-          'SourceFile' => '{{ veracode[evidence.sourcefile] }}'
-        },
-        'issue' => {
-          'Title' => '{{ veracode[issue.cwename] }}',
-          'CVSSv3.BaseScore' => 'n/a',
-          'CVSSv3.Vector' => 'n/a',
-          'Type' => 'Internal',
-          'Description' => '{{ veracode[issue.description] }}',
-          'Solution' => '{{ veracode[issue.remediation_status] }}',
-          'References' => 'https://cwe.mitre.org/data/definitions/{{ veracode[issue.cweid] }}.html',
-          'Severity' => '{{ veracode[issue.severity] }}',
-          'Category' => '{{ veracode[issue.categoryname] }}',
-          'CWE' => '{{ veracode[issue.cweid] }}',
-          'RemediationStatus' => '{{ veracode[issue.remediation_status] }}'
-        }
+    DEFAULT_MAPPING = {
+      evidence: {
+        'Description' => '{{ veracode[evidence.description] }}',
+        'LineNumber' => '{{ veracode[evidence.line] }}',
+        'SourceFile' => '{{ veracode[evidence.sourcefile] }}'
+      },
+      issue: {
+        'Title' => '{{ veracode[issue.cwename] }}',
+        'CVSSv3.BaseScore' => 'n/a',
+        'CVSSv3.Vector' => 'n/a',
+        'Type' => 'Internal',
+        'Description' => '{{ veracode[issue.description] }}',
+        'Solution' => '{{ veracode[issue.remediation_status] }}',
+        'References' => 'https://cwe.mitre.org/data/definitions/{{ veracode[issue.cweid] }}.html',
+        'Severity' => '{{ veracode[issue.severity] }}',
+        'Category' => '{{ veracode[issue.categoryname] }}',
+        'CWE' => '{{ veracode[issue.cweid] }}',
+        'RemediationStatus' => '{{ veracode[issue.remediation_status] }}'
       }
-    end
+    }.freeze
   end
 end
